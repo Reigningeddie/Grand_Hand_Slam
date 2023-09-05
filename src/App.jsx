f1898889785c18201e727ba2af0f032d5590a5cc
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-05 12:51:38 -0700

    intial commit -created home component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {HashRouter} from 'react-router-dom'
+import Home from './components/home'
+
+function App() {
+  return (
+  <HashRouter>
+    <Home />
+  </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <h1>Grand Hand Slam</h1>
+    </main>
+    </>
+  )
+}
