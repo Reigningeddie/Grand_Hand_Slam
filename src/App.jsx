1c83081bc38389cdccc242e8ea728cfca1886957
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-07 19:21:06 -0700

    created banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {HashRouter} from 'react-router-dom'
 import Home from './components/home'
 import Nav from './components/nav'
+import Banner from './components/banner'
 
 function App() {
   return (
   <HashRouter>
+    <Banner />
     <Nav />
     <Home />
   </HashRouter>
