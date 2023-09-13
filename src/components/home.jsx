5b1b7e60b919cc46857dca6ea4e130a83f992f67
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-13 16:16:05 -0700

    added grid to home component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,9 +5,16 @@ export default function Home() {
     <main className={css.body}>
       <h1 className={css.title}>Grand Hand Slam</h1>
       <p className={css.pic}></p>
-      <p className={css.name}>User Name</p>
+      <h4 className={css.name}>User Name</h4>
+      <p className={css.grid}>
+        <span className={`${css.gSum} ${css.items}`}>101</span>
+        <span className={`${css.gSum} ${css.items}`}>253</span>
+        <span className={`${css.gSum} ${css.items}`}>361</span>
+        <span className={`${css.gName} ${css.items}`}>points</span>
+        <span className={`${css.gName} ${css.items}`}>followers</span>
+        <span className={`${css.gName} ${css.items}`}>following</span>
+      </p>
       <p className={css.dez}>Description (add max characters)</p>
-      <p className={css.grid}>grid here</p>
     </main>
   )
 }
