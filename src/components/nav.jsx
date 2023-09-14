a032f14f36234c3e730466655c6549bfcf2d2577
Eduardo Aguilar <eaguilar92105@yahoo.com>
2023-09-13 18:43:53 -0700

    working on nav grid css

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,14 @@ export default function Nav() {
   return(
     <p className={css.body}>
     <p className={`${css.structure} ${css.sides}`}></p>
-      <main className={`${css.structure} ${css.main}`}>main</main>
+      <main className={`${css.structure} ${css.main}`}>
+        <container className={css.grid}>
+          <span className={css.item}>No.1</span>
+          <span className={css.item}>No.2</span>
+          <span className={css.item}>No.3</span>
+          <span className={css.item}>No.4</span>
+        </container>
+      </main>
     <p className={`${css.structure} ${css.sides}`}></p>
     </p>
   )
